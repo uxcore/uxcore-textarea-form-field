8e947da1f809570544d3c872f33b8f66d9ba33f7
光弘 <guanghong.wsj@alibaba-inc.com>
2016-12-29 11:18:39 +0800

    ver. 0.1.8

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # history
 
+## 0.1.7
+
+`CHANGED` add special className for Mount
+
 ## 0.1.6
 
 `CHANGED` add SubComp `TextAreaCount`
--- src/TextareaFormField.jsx
+++ src/TextareaFormField.jsx
@@ -15,12 +15,26 @@ const classnames = require('classnames');
 
 const trim = str => str.replace(/(^\s+|\s+$)/g, '');
 
-const FormCount = props => <div className="kuma-uxform-textarea-count">{`${props.length}/${props.total}`}</div>;
+const FormCount = props => <div className="kuma-uxform-textarea-count">
+  <span
+    className={classnames({
+      [`${props.prefixCls}-actual`]: true,
+      [`${props.prefixCls}-overflow`]: parseInt(props.length, 10) > parseInt(props.total, 10),
+    })}
+  >{props.length}</span>
+  <span className={`${props.prefixCls}-slash`}>/</span>
+  <span className={`${props.prefixCls}-max`}>{props.total}</span>
+</div>;
 
 FormCount.displayName = 'FormCount';
 FormCount.propTypes = {
+  prefixCls: React.PropTypes.string,
   length: React.PropTypes.number,
   total: React.PropTypes.number,
+};
+
+FormCount.defaultProps = {
+  prefixCls: 'kuma-uxform-count',
 };
 
 class TextAreaFormField extends FormField {
