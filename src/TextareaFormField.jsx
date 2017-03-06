4b109e6d04229224ee3289ffe4e9c05780e0dec3
光弘 <guanghong.wsj@alibaba-inc.com>
2017-03-06 15:51:09 +0800

    ver. 0.1.11

--- src/TextareaFormField.jsx
+++ src/TextareaFormField.jsx
@@ -16,16 +16,22 @@ const { getIEVer } = require('uxcore-utils');
 
 const trim = str => str.replace(/(^\s+|\s+$)/g, '');
 
-const FormCount = props => <div className="kuma-uxform-textarea-count">
-  <span
-    className={classnames({
-      [`${props.prefixCls}-actual`]: true,
-      [`${props.prefixCls}-overflow`]: parseInt(props.length, 10) > parseInt(props.total, 10),
+const FormCount = props => (
+  <div
+    className={classnames('kuma-uxform-textarea-count', {
+      [`${props.prefixCls}-has-error`]: parseInt(props.length, 10) > parseInt(props.total, 10),
     })}
-  >{props.length}</span>
-  <span className={`${props.prefixCls}-slash`}>/</span>
-  <span className={`${props.prefixCls}-max`}>{props.total}</span>
-</div>;
+  >
+    <span
+      className={classnames({
+        [`${props.prefixCls}-actual`]: true,
+        [`${props.prefixCls}-overflow`]: parseInt(props.length, 10) > parseInt(props.total, 10),
+      })}
+    >{props.length}</span>
+    <span className={`${props.prefixCls}-slash`}>/</span>
+    <span className={`${props.prefixCls}-max`}>{props.total}</span>
+  </div>
+);
 
 FormCount.displayName = 'FormCount';
 FormCount.propTypes = {
